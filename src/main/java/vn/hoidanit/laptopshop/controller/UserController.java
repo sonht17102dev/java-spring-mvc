0cd9daee363c60dbe1baa08d3296ffe39b52c737
sonht17102 <sonhtfx17102@funix.edu.vn>
2025-04-28 23:50:18 +0700

    config view

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -0,0 +1,26 @@
+package vn.hoidanit.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.laptopshop.service.UserService;
+
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String getUserPage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("test", test);
+        return "hello";
+    }
+}
